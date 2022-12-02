0108e3b4d9dab1661e0c200cd24375aeb37aad20
Optio1 <Agentoptio@gmail.com>
2022-12-01 20:40:54 -0600

    Update DemoApplicationTests.java
    Deklan Glaser
    Dec 1 2022

--- demo 3/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo 3/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -9,5 +9,11 @@ class DemoApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	@Test
+	void testGetContent() {
+		Greeting greeting = new Greeting(1,"Test");
+		assertEquals("Test", greeting.getContent());
+	}
 
 }
